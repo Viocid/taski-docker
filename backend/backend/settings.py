ce8e9f5af51de064acbaec85b069e50015fbb6ee
Konstantin <versalione@gmail.com>
2024-09-28 18:05:03 +0300

    Final_V4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['84.201.176.154', '127.0.0.1', 'localhost', 'versalione.ddns.net']
 
 
 # Application definition
